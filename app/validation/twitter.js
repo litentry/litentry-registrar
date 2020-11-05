19828ea4126e43266fbed80f8a8b1a12a2988ad2
Han Zhao <windzhaohan@gmail.com>
2020-11-05 16:33:11 +0100

    Added twitter api calls for DM

--- app/validation/twitter.js
+++ app/validation/twitter.js
@@ -1,5 +1,27 @@
 'use strict';
 
 const TwitterApi = require('twitter');
+const Configs = require('dotenv').config();
 
-const twitter = new TwitterApi();
+const client = new TwitterApi({
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET,
+  access_token_key: process.env.TOKEN_KEY,
+  access_token_secret: process.env.TOKEN_SECRET
+});
+
+var params = {screen_name: 'nodejs'};
+client.get('direct_messages/welcome_message/list', params, function(error, tweets, response) {
+  if (!error) {
+    console.log(response);
+  }else{
+    console.error(error);
+  }
+});
+client.get('direct_messages/events/show.json?id=1324362009534734340', params, function(error, tweets, response) {
+  if (!error) {
+    console.log(response);
+  }else{
+    console.error(error);
+  }
+});
