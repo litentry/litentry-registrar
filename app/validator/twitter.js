3a72ffdd421a547a8ca9043601c3b1de35e9dd96
Han Zhao <windzhaohan@gmail.com>
2020-11-09 15:38:23 +0100

    Restructured code

--- app/validator/twitter.js
+++ app/validator/twitter.js
@@ -2,6 +2,7 @@
 
 const TwitterApi = require('twitter');
 const Configs = require('dotenv').config();
+const Validator = require('app/validator/base');
 
 if (Configs.error) {
   throw Configs.error;
@@ -13,20 +14,29 @@ const client = new TwitterApi({
   access_token_key: process.env.TOKEN_KEY,
   access_token_secret: process.env.TOKEN_SECRET
 });
-
 const params = { screen_name: 'nodejs' };
 
-client.get('direct_messages/events/list', params, function (error, msgs) {
-  if (!error) {
-    console.log(msgs);
-  } else {
-    console.error(error);
+class TwitterValidator extends Validator {
+  constructor(config) {
+    super(config);
   }
+  async invoke(userId, emailAddr) {
 
-  if(msgs.events) {
-    for(const event of msgs.events) {
-      console.log(event.message_create.message_data.text)
-    }
+    client.get('direct_messages/events/list', params, function (error, msgs) {
+      if (!error) {
+        console.log(msgs);
+      } else {
+        console.error(error);
+      }
+
+      if (msgs.events) {
+        for (const event of msgs.events) {
+          console.log(event.message_create.message_data.text)
+        }
+      }
+
+    });
   }
+}
 
-});
+module.exports = TwitterValidator;
