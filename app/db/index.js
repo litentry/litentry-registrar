6ce6719864ae8d47a1f8078474e53e4f037749ca
zongxiongchen <czxczf@gmail.com>
2020-11-12 14:44:40 +0100

    Remove in-memory database, abstraction for requestJudgement collection

--- app/db/index.js
+++ app/db/index.js
@@ -1,62 +1,11 @@
 'use strict';
 
-const _ = require('lodash');
-const { v4: uuidv4 } = require('uuid');
+
 const MongoClient = require('mongodb').MongoClient;
 
 const config = require('app/config').mongodb;
 const logger = require('app/logger');
 
-
-class InMemoryStorage {
-    constructor(config) {
-        this.config = config;
-        this.database = {};
-    }
-    async connect() {
-
-    }
-
-    async query(collection, filter) {
-        const _collection = this.database[collection];
-        const keys = _.keys(filter);
-
-        for (let _content of _collection) {
-            let cnt = 0;
-            for (let key of keys) {
-                if (_content[key] && filter[key] != _content[key]) {
-                    // one of values is not the same
-                    break;
-                } else {
-                    cnt += 1;
-                }
-            }
-            if (cnt === keys.length) {
-                return _content;
-            }
-        }
-
-        return null;
-    }
-    async insert(collection, content) {
-        const id = uuidv4();
-        if (! this.database[collection]) {
-            this.database[collection] = [{ ...content, _id: id }];
-        } else {
-            this.database[collection].push({ ...content, _id: id });
-        }
-        return id;
-    }
-    async updateById(collection, id, content) {
-        const _collection = this.database[collection];
-        for (let _content of _collection) {
-            if (_content._id === id) {
-                _.merge(_content, content);
-                break;
-            }
-        }
-    }
-}
 
 class MongodbStorage {
     constructor(config) {
@@ -65,6 +14,11 @@ class MongodbStorage {
         this.database = null;
     }
 
+    /**
+     * Connect to mongodb
+     * TODO: We need to make sure this connection is correct and handle some exceptions if
+     *       errors occur.
+     */
     async connect() {
         if (this.client) {
             logger.debug(`[MongodbStorage.connect] storage is already connected.`);
@@ -78,14 +32,11 @@ class MongodbStorage {
         } else {
             endpoint = `mongodb://${this.config.host}:${this.config.port}`;
         }
-        try {
-            this.client = new MongoClient(endpoint);
-            await this.client.connect();
-            this.database = this.client.db(this.config.dbName);
-        } catch (error) {
-            logger.error(`[Storage.connect] unexpected error occurs: ${error}`);
-            console.trace(error);
-        }
+
+        this.client = new MongoClient(endpoint);
+        await this.client.connect();
+        this.database = this.client.db(this.config.dbName);
+        logger.info(`[MongodbStorage.connect] connect to mongodb successfully.`);
     }
 
     /**
@@ -96,8 +47,12 @@ class MongodbStorage {
     async query(collection, filter) {
         await this.connect();
         const _collection = this.database.collection(collection);
-        const result = await _collection.findOne(filter);
-        return result;
+        const results = await _collection.find(filter).toArray();
+        return results;
+    }
+
+    async queryById(collection, id) {
+        return await this.query(collection, { _id: id });
     }
 
     /**
@@ -113,7 +68,6 @@ class MongodbStorage {
         return result.insertedId;
     }
 
-
     /**
      * @param {String} collection - the collection to be queried inside mongodb database
      * @param {*} filter
@@ -121,38 +75,93 @@ class MongodbStorage {
      * @return {String} return insertedKey associated with this document
      */
     async update(collection, filter, content) {
-        // const filter = { _id: id };
+        await this.connect();
+
         const options = { upsert: true };
         const updateDoc = { $set: content };
         const _collection = this.database.collection(collection);
         const result = await _collection.updateOne(filter, updateDoc, options);
-
-        logger.debug(`[MongodbStorage.updateByID] update ${result.insertedCount} document into ${collection}, insertedId is: ${result.insertedId}`);
-        return result.insertedId;
+        logger.debug(`[MongodbStorage.update] update ${result.modifiedCount} document into ${collection}`);
     }
-
     async updateById(collection, id, content) {
         const filter = { _id: id };
         return await this.update(collection, filter, content);
     }
-
-    async updateByEmail(collection, email, content) {
-        const filter = { email: email };
-        return await this.update(collection, filter, content);
-    }
-    /* eslint-disable-next-line */
-    async updateByRiot(collection, riot, content) {
-        // TODO:
-    }
-    /* eslint-disable-next-line */
-    async updateByTwitter(collection, twitter, content) {
-        // TODO:
-    }
 }
 
+class RequestJudgementCollection {
+    constructor(db) {
+        this.db = db;
+        this.collectionName = 'requestJudgement';
+    }
+    async insert(content) {
+        return await this.db.insert(this.collectionName, content);
+    }
+    async updateById(id, content) {
+        return await this.db.updateById(this.collectionName, id, content);
+    }
+    async updateByAccount(account, content) {
+        const filter = { account: account };
+        return await this.db.update(this.collectionName, filter, content);
+    }
 
-const storage = config ? new MongodbStorage(config) : new InMemoryStorage(config);
+    async query(filter) {
+        // TOOD: find one result or all result
+        const results = await this.db.query(this.collectionName, filter);
+        return results[0];
+    }
+
+    async queryByAccount(account) {
+        return await this.query(this.collectionName, { account: account });
+    }
+
+    async setEmailVerifiedPending(account, email, addition={}) {
+        const filter = { account: account, email: email };
+        const content = { emailStatus: 'pending', ...addition };
+        return await this.db.update(this.collectionName, filter, content);
+    }
+    async setEmailVerifiedSuccess(account, email) {
+        const filter = { account: account, email: email };
+        const content = { emailStatus: 'verifiedSuccess' };
+        return await this.db.update(this.collectionName, filter, content);
+    }
+    async setEmailVerifiedFailed(account, email) {
+        const filter = { account: account, email: email };
+        const content = { emailStatus: 'verifiedFailed' };
+        return await this.db.update(this.collectionName, filter, content);
+    }
+
+    async setTwitterVerifiedSuccess(account, twitter) {
+        const filter = { account: account, twitter: twitter };
+        const content = { twitterStatus: 'verifiedSuccess' };
+        return await this.db.update(this.collectionName, filter, content);
+    }
+    async setTwitterVerifiedFailed(account, twitter) {
+        const filter = { account: account, twitter: twitter };
+        const content = { twitterStatus: 'verifiedFailed' };
+        return await this.db.update(this.collectionName, filter, content);
+    }
+
+    async setRiotVerifiedSuccess(account, riot) {
+        const filter = { account: account, riot: riot };
+        const content = { riotStatus: 'verifiedSuccess' };
+        return await this.db.update(this.collectionName, filter, content);
+    }
+    async setRiotVerifiedFailed(account, riot) {
+        const filter = { account: account, riot: riot };
+        const content = { riotStatus: 'verifiedFailed' };
+        return await this.db.update(this.collectionName, filter, content);
+    }
+
+}
+
+if (! config) {
+    throw new Error('Add configuration for mongodb.');
+}
+
+const storage = new MongodbStorage(config);
 
 module.exports = {
-    Storage: storage
+    Storage: storage,
+    RequestJudgementCollection: new RequestJudgementCollection(storage)
 };
