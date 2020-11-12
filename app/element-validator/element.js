8868d402ad375a58b43e0b5f2fa3e09bb2a2dd8a
Shihao <willzhaoshihao@gmail.com>
2020-11-12 13:11:13 -0500

    Element validation - add periodic checking function to elememt validation

--- app/element-validator/element.js
+++ app/element-validator/element.js
@@ -6,26 +6,63 @@
 const axios = require('axios');
 const config = require('./dev_element_config');
 
+let lastReadEventID;
+const TARGET_MESSAGE_FOUND = 'TARGET_MESSAGE_FOUND';
+let isTargetMessageFoundFlag = false;
 
-function checkTargetMessageFromHistory(targetUserId, targetMessage) {
-    getRoomEventHistory(targetUserId, targetMessage,'', 0, true, '');
+
+function startCheckingTargetMessage(targetUserId, targetMessage, interval=120000, maxWaitingTime=1800000) {
+    checkTargetMessageFromHistory(targetUserId, targetMessage, lastReadEventID);
+    //Provide 2 sec for checkTargetMessageFromHistory to process and make signal
+    setTimeout(function() {  
+        setupIntervalCheck(targetUserId, targetMessage, interval, maxWaitingTime);
+    }, 2000);
 }
 
-async function getRoomEventHistory(targetUserId, targetMessage, nextSyncToken, page, isFirstCall, lastReadEventId) {
+//Default interval: 2 min; default waiting time: 30 min
+function setupIntervalCheck(targetUserId, targetMessage, interval=120000, maxWaitingTime=1800000) {
+    if (isTargetMessageFoundFlag) {
+            console.log("The target message was found on first call, quiting...")
+            return;
+        }
+        else {
+            let caller = setInterval(function(){
+                if (isTargetMessageFoundFlag) {
+                    console.log("The target message was found, quiting...")
+                    clearInterval(caller);
+                    clearTimeout(timeout);
+                    return;
+                } else {
+                    checkTargetMessageFromHistory(targetUserId, targetMessage, lastReadEventID);
+                }
+    
+            }, interval);
+            let timeout = setTimeout(function() {
+                clearInterval(caller);
+                console.log("clearInterval...")
+                return;
+            }, maxWaitingTime-2000);
+        }
+}
 
-    console.log('This is stream no.' + page)
-    await requestRoomEventHistory(nextSyncToken).then(
+function checkTargetMessageFromHistory(targetUserId, targetMessage, lastReadEventId='', nextSyncToken='', page=0, isFirstCall=true) {
+
+    console.log('This is stream no.' + page);
+    requestRoomEventHistory(nextSyncToken).then(
         function(result) {
             if (result.data.start == result.data.end) {
                 console.log('You have reached the end of room event history');
                 return;
             }
             else {
+                if (page == 0) {
+                    lastReadEventID = result.data.chunk[0].event_id;
+                }
                 let i;
                 for (i=0; i<result.data.chunk.length; i++) {
                     let cur_event = result.data.chunk[i];
                     if (cur_event.event_id == lastReadEventId) {
-                        console.log('There is no more new event');
+                        console.log('There is no more unread event');
                         return;
                     }
                     if (cur_event.type == 'm.room.message') {
@@ -33,16 +70,15 @@ async function getRoomEventHistory(targetUserId, targetMessage, nextSyncToken, p
                         console.log(cur_event.content);
                     }
                     if (cur_event.user_id == targetUserId && cur_event.content.body == targetMessage) {
+                        isTargetMessageFoundFlag = true;
                         console.log('And congratulations! You have found the target message!');
                         return;
                     }
                 }
-                //Only request the next page/steam of the 10 events, if this is not the first call
-                // if (!isFirstCall) {
                 if (page <= 5) {
                     nextSyncToken = result.data.end;
                     page += 1;
-                    return getRoomEventHistory(targetUserId, targetMessage, nextSyncToken, page, false, lastReadEventId);
+                    return checkTargetMessageFromHistory(targetUserId, targetMessage, lastReadEventId, nextSyncToken, page, false);
                 }
             }
         },
@@ -63,4 +99,4 @@ async function requestRoomEventHistory(nextSyncToken) {
 
 // getRoomEventHistory('','',``, 0, false, '');
 // checkTargetMessageFromHistory('@testingshark:matrix.org', 'hlel');
-
+startCheckingTargetMessage('@testingshark:matrix.org', 'hello', 3000, 60000);
