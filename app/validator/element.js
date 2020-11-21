121540e8ea1a67e6d85e41b9c1bfd42db60cf0f9
Shihao <willzhaoshihao@gmail.com>
2020-11-21 03:11:54 -0500

    Reorganize the element polling function in validator class

--- app/validator/element.js
+++ app/validator/element.js
@@ -6,17 +6,127 @@ const logger = require('app/logger');
 const Validator = require('app/validator/base');
 const { ValidatorEvent } = require('app/validator/events');
 const { RequestJudgementCollection } = require('app/db');
-// const utils = require('app/utils');
-let lastReadEventID;
-let isTargetMessageFoundFlag = false;
 
 class ElementValidator extends Validator {
     constructor(config) {
         super(config);
+        this.lastReadEventID = '';
+        this.isTargetMessageFoundFlag = false;
     }
 
     async invoke(targetUser, targetWalletAddr, databaseId) {
-        await startCheckingTargetMessage(targetUser, targetWalletAddr, databaseId);
+        this.startCheckingTargetMessage(targetUser, targetWalletAddr, databaseId);
+    }
+
+    startCheckingTargetMessage(
+        targetUserId,
+        targetMessage,
+        databaseId,
+        interval = config.elementValidator.pollingInterval,
+        maxWaitingTime = config.elementValidator.maxPollingTime
+    ) {
+        this.checkTargetMessageFromHistory(targetUserId, targetMessage, databaseId, this.lastReadEventID, true);
+        this.setupIntervalCheck(targetUserId, targetMessage, databaseId, interval, maxWaitingTime);
+    }
+    
+    //Default interval: 5 sec; default waiting time: 50 sec
+    setupIntervalCheck(targetUserId, targetMessage, databaseId, interval=5000, maxWaitingTime=50000) {
+        const self = this;
+        const caller = setInterval(async function () {
+            await self.checkTargetMessageFromHistory(targetUserId, targetMessage, databaseId, self.lastReadEventID, false);
+            //Provide 3 sec for checkTargetMessageFromHistory to process and make signal
+            setTimeout(function () {
+                if (self.isTargetMessageFoundFlag) {
+                    logger.debug('Riot polling: The target message was found, quiting...');
+                    clearInterval(caller);
+                    clearTimeout(timeout);
+                    return;
+                }
+            }, 3000);
+        }, interval);
+    
+        const timeout = setTimeout(async function () {
+            clearInterval(caller);
+            await RequestJudgementCollection.setRiotVerifiedFailedById(databaseId);
+            logger.debug(
+                `Riot polling for user ${targetUserId} reached time out, set verification as failed and clear interval...`
+            );
+            return;
+        }, maxWaitingTime);
+    }
+    
+    async checkTargetMessageFromHistory(
+        targetUserId,
+        targetMessage,
+        databaseId,
+        lastReadEventId = '',
+        isFirstCall = true,
+        nextSyncToken = '',
+        page = 0
+    ) {
+        // logger.debug('This is stream no.' + page);
+        try {
+            const result = await this.requestRoomEventHistory(nextSyncToken);
+            if (result) {
+                if (result.data.start === result.data.end) {
+                    logger.debug('Riot polling: You have reached the end of room event history');
+                    return;
+                } else {
+                    if (page === 0) {
+                        this.lastReadEventID = result.data.chunk[0].event_id;
+                    }
+                    let i;
+                    for (i = 0; i < result.data.chunk.length; i++) {
+                        let cur_event = result.data.chunk[i];
+                        if (cur_event.event_id === lastReadEventId) {
+                            logger.debug('Riot polling: There is no more unread event');
+                            return;
+                        }
+                        if (cur_event.type === 'm.room.message') {
+                            // console.log('------ ' + cur_event.user_id + ' said:');
+                            // console.log(cur_event.content);
+                            if (cur_event.user_id === targetUserId && cur_event.content.body === targetMessage) {
+                                this.isTargetMessageFoundFlag = true;
+                                await RequestJudgementCollection.setRiotVerifiedSuccessById(databaseId);
+                                logger.debug('Riot polling: ------ ' + cur_event.user_id + ' said:');
+                                logger.debug('Riot polling:' + cur_event.content.body);
+                                logger.debug('Riot polling: And congratulations! You have found the target message!');
+                                return;
+                            }
+                        }
+                    }
+    
+                    if (!isFirstCall) {
+                        nextSyncToken = result.data.end;
+                        page += 1;
+                        return this.checkTargetMessageFromHistory(
+                            targetUserId,
+                            targetMessage,
+                            databaseId,
+                            lastReadEventId,
+                            false,
+                            nextSyncToken,
+                            page
+                        );
+                    }
+                }
+            } else {
+                logger.debug(`Riot polling failed, retry polling...`);
+            }
+        } catch (err) {
+            logger.error('Riot polling: ' + err);
+        }
+    }
+    
+    async requestRoomEventHistory(nextSyncToken) {
+        const hostUrl = config.elementValidator.hostUrl;
+        const roomId = config.elementValidator.roomId;
+        const accessToken = config.elementValidator.accessToken;
+        const allMessages =
+            hostUrl +
+            `/_matrix/client/r0/rooms/${roomId}/messages?limit=10&from=${nextSyncToken}&access_token=${accessToken}&dir=b`;
+        const res = await axios.get(allMessages);
+        return res;
     }
 }
 
@@ -31,113 +141,3 @@ ValidatorEvent.on('handleRiotVerification', async (info) => {
 });
 
 module.exports = elementValidator;
-
-function startCheckingTargetMessage(
-    targetUserId,
-    targetMessage,
-    databaseId,
-    interval = config.elementValidator.pollingInterval,
-    maxWaitingTime = config.elementValidator.maxPollingTime
-) {
-    checkTargetMessageFromHistory(targetUserId, targetMessage, databaseId, lastReadEventID, true);
-    setupIntervalCheck(targetUserId, targetMessage, databaseId, interval, maxWaitingTime);
-}
-
-//Default interval: 2 min; default waiting time: 30 min
-function setupIntervalCheck(targetUserId, targetMessage, databaseId, interval=5000, maxWaitingTime=50000) {
-    let caller = setInterval(async function () {
-        await checkTargetMessageFromHistory(targetUserId, targetMessage, databaseId, lastReadEventID, false);
-        //Provide 3 sec for checkTargetMessageFromHistory to process and make signal
-        setTimeout(function () {
-            if (isTargetMessageFoundFlag) {
-                logger.debug('Riot polling: The target message was found, quiting...');
-                clearInterval(caller);
-                clearTimeout(timeout);
-                return;
-            }
-        }, 3000);
-    }, interval);
-
-    let timeout = setTimeout(async function () {
-        clearInterval(caller);
-        await RequestJudgementCollection.setRiotVerifiedFailedById(databaseId);
-        logger.debug(
-            `Riot polling for user ${targetUserId} reached time out, set verification as failed and clear interval...`
-        );
-        return;
-    }, maxWaitingTime);
-}
-
-async function checkTargetMessageFromHistory(
-    targetUserId,
-    targetMessage,
-    databaseId,
-    lastReadEventId = '',
-    isFirstCall = true,
-    nextSyncToken = '',
-    page = 0
-) {
-    // console.log('This is stream no.' + page);
-    try {
-        const result = await requestRoomEventHistory(nextSyncToken);
-        if (result) {
-            if (result.data.start === result.data.end) {
-                logger.debug('Riot polling: You have reached the end of room event history');
-                return;
-            } else {
-                if (page === 0) {
-                    lastReadEventID = result.data.chunk[0].event_id;
-                }
-                let i;
-                for (i = 0; i < result.data.chunk.length; i++) {
-                    let cur_event = result.data.chunk[i];
-                    if (cur_event.event_id == lastReadEventId) {
-                        logger.debug('Riot polling: There is no more unread event');
-                        return;
-                    }
-                    if (cur_event.type === 'm.room.message') {
-                        if (cur_event.user_id === targetUserId && cur_event.content.body === targetMessage) {
-                            isTargetMessageFoundFlag = true;
-                            await RequestJudgementCollection.setRiotVerifiedSuccessById(databaseId);
-                            logger.debug('Riot polling: ------ ' + cur_event.user_id + ' said:');
-                            logger.debug('Riot polling: ' + cur_event.content);
-                            logger.debug('Riot polling: And congratulations! You have found the target message!');
-                            return;
-                        }
-                    }
-                }
-
-                if (!isFirstCall) {
-                    nextSyncToken = result.data.end;
-                    page += 1;
-                    return checkTargetMessageFromHistory(
-                        targetUserId,
-                        targetMessage,
-                        databaseId,
-                        lastReadEventId,
-                        false,
-                        nextSyncToken,
-                        page
-                    );
-                }
-            }
-        } else {
-            logger.debug(`Riot polling failed, retry polling...`);
-        }
-    } catch (err) {
-        logger.error('Riot polling: ' + err);
-    }
-}
-
-async function requestRoomEventHistory(nextSyncToken) {
-    const hostUrl = config.elementValidator.hostUrl;
-    const roomId = config.elementValidator.roomId;
-    const accessToken = config.elementValidator.accessToken;
-    const allMessages =
-        hostUrl +
-        `/_matrix/client/r0/rooms/${roomId}/messages?limit=10&from=${nextSyncToken}&access_token=${accessToken}&dir=b`;
-    const res = await axios.get(allMessages);
-    return res;
-}
-
-// startCheckingTargetMessage('@testingshark:matrix.org', 'hello testing', 15000, 120000);
