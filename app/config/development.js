b26fb1451d6a1adb1dae0c71710ebfdded7b2d4c
zongxiong.chen <czxczf@gmail.com>
2021-02-15 17:14:48 +0100

    Add twitter cron job

--- app/config/development.js
+++ app/config/development.js
@@ -32,7 +32,7 @@ module.exports = Object.freeze({
         apiKey: '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',
         username: 'no-reply@litentry.com',
         subject: 'Validation From Litentry',
-        jobInterval: 60 * 30,        //  seconds
+        jobInterval: 30,        //  seconds
     },
 
     elementValidator: {
@@ -41,8 +41,7 @@ module.exports = Object.freeze({
         accessToken: 'MDAxOGxvY2F0aW9uIG1hdHJpeC5vcmcKMDAxM2lkZW50aWZpZXIga2V5CjAwMTBjaWQgZ2VuID0gMQowMDJiY2lkIHVzZXJfaWQgPSBAbGl0ZW50cnktYm90Om1hdHJpeC5vcmcKMDAxNmNpZCB0eXBlID0gYWNjZXNzCjAwMjFjaWQgbm9uY2UgPSA3fmxqWWorOXc1WnBGb0lNCjAwMmZzaWduYXR1cmUg3oXZA9SOsJE2bXUEnelgKxIbFJrqn5kFzv1LHgk3U4UK',
         userId: '@litentry-bot:matrix.org',
         homeServerUrl: 'https://matrix-client.matrix.org',
-        jobInterval: 60 * 30,        //  seconds
-
+        jobInterval: 30,        //  seconds
     },
     twitterValidator: {
         callbackEndpoint: 'http://localhost:8080/callback/validationTwitter',
@@ -51,7 +50,7 @@ module.exports = Object.freeze({
         bearerToken: 'AAAAAAAAAAAAAAAAAAAAAL3XJQEAAAAAKFKqbRv%2BU4cjnyOOpAzO%2B0yvg0A%3D2Y8wAh6p9LWJ5ZqenwPDRXikQK8BalIqHC2uHC3pS1OxAJ9c85',
         accessToken: '1324028605245083652-i0SQABpfeQbf5gmkVwvvEqhPMc6zKA',
         accessTokenSecret: 'rmJSmMSHffb7j0yixbI2AOreFbZ4ABSWMrCJs77PfsabF',
-        jobInterval: 60 * 30,        //  seconds
+        jobInterval: 30,        //  seconds
     },
     jwt: {
         sessionSecret: '@v96a%%3s_5gBBMW',
--- app/jobs/index.js
+++ app/jobs/index.js
@@ -1,9 +1,11 @@
 const EmailJob = require('app/jobs/email');
 const ElementJob = require('app/jobs/element');
+const TwitterJob = require('app/jobs/twitter');
 const ProvideJudgementJob = require('app/jobs/provideJudgement');
 
 module.exports = {
     EmailJob: EmailJob,
     ElementJob: ElementJob,
+    TwitterJob: TwitterJob,
     ProvideJudgementJob: ProvideJudgementJob,
 };
