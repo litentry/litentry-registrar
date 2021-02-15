5dbb29ad99379be62abcf3d1727c4a0f0d81084c
zongxiong.chen <czxczf@gmail.com>
2021-02-15 17:20:15 +0100

    Remove useless endpoints

--- app/router/index.js
+++ app/router/index.js
@@ -5,82 +5,10 @@ const app = require('express').Router();
 const logger = require('app/logger');
 const validator = require('app/validator');
 const Chain = require('app/chain');
-const { createJwtToken, decodeJwtToken, generateNonce } = require('app/utils');
+const { decodeJwtToken } = require('app/utils');
 const { RequestJudgementCollection, RiotCollection } = require('app/db');
 
 const REDIRECT_URL = 'https://www.litentry.com';
-
-app.get('/', async (req, res) => {
-    try {
-        return res.json({ status: 'success', msg: 'Hello world (Just for debug, will be removed in the future).' });
-    } catch (error) {
-        logger.error(`GET / unexcepected error ${JSON.stringify(error)}`);
-        console.trace(error);
-
-        res.status(400);
-        return res.json({ status: 'fail', msg: new String(error) });
-    }
-});
-
-app.post('/chain/eventListener/start', async (req, res) => {
-    try {
-        let handler = await Chain.eventListenerStart();
-        if (handler) {
-            return res.json({ status: 'success', msg: 'Event Listener starts successfully.' });
-        } else {
-            return res.json({ status: 'fail', msg: 'Event Listener fails to start successfully.' });
-        }
-    } catch (error) {
-        logger.error(`GET /chain/event-listneer/start unexcepected error ${JSON.stringify(error)}`);
-        console.trace(error);
-
-        res.status(400);
-        return res.json({ status: 'fail', msg: new String(error) });
-    }
-});
-
-app.post('/chain/eventListener/stop', async (req, res) => {
-    try {
-        Chain.eventListenerStop();
-        return res.json({ status: 'success', msg: 'Event Listener stops successfully.' });
-    } catch (error) {
-        logger.error(`GET /chain/eventListener/stop unexcepected error ${JSON.stringify(error)}`);
-        console.trace(error);
-
-        res.status(400);
-        return res.json({ status: 'fail', msg: new String(error) });
-    }
-});
-
-app.post('/chain/eventListener/restart', async (req, res) => {
-    try {
-        Chain.eventListenerRestart();
-        return res.json({ status: 'success', msg: 'Event Listener stops successfully.' });
-    } catch (error) {
-        logger.error(`GET /chain/eventListener/restart unexcepected error ${JSON.stringify(error)}`);
-        console.trace(error);
-
-        res.status(400);
-        return res.json({ status: 'fail', msg: new String(error) });
-    }
-});
-
-app.get('/chain/eventListener/status', async (req, res) => {
-    try {
-        // FIXME: It's a silly implementation.
-        if (Chain.unsubscribeEventListener) {
-            return res.json({ status: 'success', msg: 'Running.' });
-        } else {
-            return res.json({ status: 'success', msg: 'Stop.' });
-        }
-    } catch (error) {
-        logger.error(`GET /chain/eventListener/status unexcepected error ${JSON.stringify(error)}`);
-        console.trace(error);
-
-        res.status(400);
-        return res.json({ status: 'fail', msg: new String(error) });
-    }
-});
 
 app.post('/chain/provideJudgement', async (req, res) => {
     try {
@@ -96,25 +24,6 @@ app.post('/chain/provideJudgement', async (req, res) => {
         return res.json({ status: 'fail', msg: new String(error) });
     }
 });
-
-// app.post('/validate/email', async (req, res) => {
-//     try {
-//         const { email } = req.body;
-
-//         const nonce = generateNonce();
-//         const token = createJwtToken({ email: email, nonce: nonce });
-
-//         await validator.EmailValidator.invoke(email, token);
-
-//         return res.json({ status: 'success', msg: '' });
-//     } catch (error) {
-//         logger.error(`POST /validate/email unexcepected error ${JSON.stringify(error)}`);
-//         console.trace(error);
-
-//         res.status(400);
-//         return res.json({ status: 'fail', msg: new String(error) });
-//     }
-// });
 
 app.get('/callback/validationEmail', async (req, res) => {
     try {
@@ -189,7 +98,6 @@ app.get('/callback/validationElement', async (req, res) => {
         return res.redirect(REDIRECT_URL);
     }
 });
-
 
 app.get('/callback/validationTwitter', async (req, res) => {
     try {
