71f8993892a5b3cd81f39d296cb2b57be67a9596
zxchen <czxczf@gmail.com>
2021-04-21 00:44:15 +0200

    Add ci script & refctor setup.js (#35)
    * Add ci script & refctor setup.js
    
    * Update staging configuration
    
    * Prettier code
    
    * CI: waiting for services
    
    * Use bob instead of alice

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 
 Litentry Polkadot/Kusama Registrar
 
-## Setup development environtment
+## Set up development environtment
 
 - Install `git-crypt`, useful commands for `git-crypt` (maybe `git crypt` on some different platforms)
 
@@ -11,11 +11,10 @@ Litentry Polkadot/Kusama Registrar
     git-crypt lock -k /path/to/key
     git-crypt unlock /path/to/key
     git-crypt status
-    git-crypt status -f
     ```
 
     See [git-crypt](https://github.com/AGWA/git-crypt) for details
-- Add a new registrar Account on our development chain
+- Add a new registrar account on our development chain
 
    ```
    node setup.js
@@ -39,34 +38,26 @@ Litentry Polkadot/Kusama Registrar
     ```
     npm run app
     ```
-    or
-
-    ```
-    npm start
-    ```
 
 - Fix import path's
   ```
   npm run link
   ```
 
-
-## Staging Server (CI)
+## Set up production environment
 
 ```
-http://ec2-13-229-136-206.ap-southeast-1.compute.amazonaws.com:8080
+npm start
 ```
+
+
+Please make sure `pm2` is contronlled by system-level process, like `systemd`.
 
 ## Chain Address
 ```
-wss://18-140-130-138
-ws://ec2-18-140-130-138.ap-southeast-1.compute.amazonaws.com:9944
+wss://testnet.litentry.io
 ```
 
-## Useful Links:
 
-```
-https://wiki.polkadot.network/docs/en/maintain-networks#westend-test-network
-https://polkadot.js.org/apps/
-https://telemetry.polkadot.io/
-```
+## How to Verify Your Identity
+If you meet any problems during identity verification, please follow the instruction given in the link [https://docs.litentry.com/registrar/HowToVerifyYourIdentity.html](https://docs.litentry.com/registrar/HowToVerifyYourIdentity.html)
--- setup.js
+++ setup.js
@@ -1,35 +1,24 @@
-process.env.NODE_ENV = 'development';
+delete process.env.NODE_ENV;
+const result = require('dotenv').config({ debug: true });
+if (result.error) {
+    throw result.error;
+}
 /**
  * See @href https://wiki.polkadot.network/docs/en/learn-identity
  */
-const _ = require('lodash');
-
-const ApiPromise = require('@polkadot/api').ApiPromise;
-const WsProvider = require('@polkadot/api').WsProvider;
-const Keyring = require('@polkadot/api').Keyring;
+const { ApiPromise, WsProvider, Keyring } = require('@polkadot/api');
 const { blake2AsHex } = require('@polkadot/util-crypto');
-const { GenericExtrinsic } = require('@polkadot/types/extrinsic/Extrinsic');
-
-console.log(GenericExtrinsic);
 const config = require('./app/config');
 
-// DEFUALT FEE is 0.1 Unit
-const DEFAULT_REGISTRAR_FEE = 100000000000000;
-const DEFAULT_DEMOCRACY_VOTE_FEE = 100000000000000;
-const DEFAULT_DEMOCRACY_PROPOSAL_FEE = 100000000000000;
+// DEFUALT FEE is 1 Unit
+const DEFAULT_REGISTRAR_FEE = 1000000000000;
+const DEFAULT_DEMOCRACY_VOTE_FEE = 1000000000000;
+const DEFAULT_DEMOCRACY_PROPOSAL_FEE = 1000000000000;
 const DEFAULT_SLEEP_INTERVAL = 6;
-
-const Judgement = {
-    Unknown: { Unknown: null },
-    Reasonable: { Reasonable: null },
-    KnownGood: { KnownGood: null },
-    OutOfDate: { OutOfDate: null },
-    LowQuality: { LowQuality: null },
-};
 
 function sleep(seconds) {
     return new Promise((resolve) => {
-        setTimeout(resolve, seconds*1000);
+        setTimeout(resolve, seconds * 1000);
     });
 }
 
@@ -40,7 +29,7 @@ class Chain {
      * @constructor
      */
     constructor(config) {
-        if (! self) {
+        if (!self) {
             self = this;
         } else {
             throw new Error(`Only one chain instance allowed`);
@@ -61,13 +50,15 @@ class Chain {
         const _bob = '//Bob';
         const _charlie = '//Charlie';
         const _dave = '//Dave';
-        if (! self.api) {
-            self.api = await ApiPromise.create({ provider: self.wsProvider,
-                                                 types: {
-                                                     Address: "MultiAddress",
-                                                     LookupSource: "MultiAddress",
-                                                 }
-                                               });
+        const _eve = '//Eve';
+        if (!self.api) {
+            self.api = await ApiPromise.create({
+                provider: self.wsProvider,
+                types: {
+                    Address: 'MultiAddress',
+                    LookupSource: 'MultiAddress',
+                },
+            });
         }
         if (!self.myself) {
             self.myself = self.keyring.addFromUri(_alice);
@@ -75,6 +66,7 @@ class Chain {
             self.bob = self.keyring.addFromUri(_bob);
             self.charlie = self.keyring.addFromUri(_charlie);
             self.dave = self.keyring.addFromUri(_dave);
+            self.eve = self.keyring.addFromUri(_eve);
         }
         return self.api;
     }
@@ -88,69 +80,6 @@ class Chain {
         }
         return null;
     }
-    /**
-     * elections
-     */
-    async electionsCandidates() {
-        await self.connect();
-        const candidates = await self.api.query.electionsPhragmen.candidates();
-        console.log(`[elections.candidates]: ${candidates.toJSON()}`);
-        return candidates;
-    }
-    async electionsMembers() {
-        await self.connect();
-        const members = await self.api.query.electionsPhragmen.members();
-        console.log(`[elections.members] ${members.toJSON()}`);
-        return members;
-    }
-    async electionsElectionRounds() {
-        await self.connect();
-        const rounds = await self.api.query.electionsPhragmen.electionRounds();
-        console.log(`[elections.electionRounds]: ${rounds}`);
-        return rounds;
-    }
-    async electionsSubmitCandidacy(account, candidateCount = 0) {
-        await self.connect();
-        // const candidateCount = 0;
-        const tx = self.api.tx.electionsPhragmen.submitCandidacy(candidateCount);
-        await self.signAndSend(tx, account);
-        console.log(`[elections.submitCandidacy]: ${tx}`);
-        return tx;
-    }
-    async electionsVote(account) {
-        await self.connect();
-        const candidates = await self.electionsCandidates();
-        // vote for 1 unit
-        const value = 1000000000000000;
-        const tx = self.api.tx.electionsPhragmen.vote(candidates, value);
-        console.log(`[elections.vote]: ${tx}`);
-        await self.signAndSend(tx, account);
-        return tx;
-    }
-
-    /**
-     * council
-     */
-    async councilMembers() {
-        await self.connect();
-        const members = await self.api.query.council.members();
-        console.log(`[council.members]: ${members}`);
-        return members;
-    }
-    async councilProposalCount() {
-        await self.connect();
-        const proposalCount = await self.api.query.council.proposalCount();
-        console.log(`[council.proposalCount]: ${proposalCount}`);
-        return proposalCount;
-    }
-    async councilProposals() {
-        await self.connect();
-        const proposals = await self.api.query.council.proposals();
-        console.log(`[council.proposals]: ${proposals}`);
-        return proposals;
-    }
-
-
     /**
      * identity
      */
@@ -172,63 +101,8 @@ class Chain {
         console.log(`[identity.addRegistrar]: ${tx}`);
         return tx;
     }
-    async identitySetIdentity(account) {
-        await self.connect();
-        let info = { display: 'Alice', email: 'test@example.com', riot: 'riot', twitter: 'twitter', web: 'http://test.com' };
-        info = _.mapValues(info, function (elem) {
-            return { Raw: elem };
-        });
-        const tx = self.api.tx.identity.setIdentity(info);
-        await self.signAndSend(tx, account);
-        console.log(`[identity.setIdentity]: ${tx}`);
-        return tx;
-    }
-    async identityRequestJudgement(account, regIndex = 0, fee = DEFAULT_REGISTRAR_FEE) {
-        // const regIndex = 0;
-        // const fee = 1;
-        const tx = self.api.tx.identity.requestJudgement(regIndex, fee);
-        await self.signAndSend(tx, account);
-        console.log(`[identity.RequestJudgement]: ${tx}`);
-        return tx;
-    }
-
-    async identityProvideJudgement(account, target = '5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY', regIndex = 0,
-                                   judgement = 'KnownGood') {
-        await self.connect();
-        // const regIndex = 0;
-        // const target = '5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY';
-        // const judgement = 'KnownGood';
-        let judgement_ = Judgement[judgement];
-
-        const tx = self.api.tx.identity.provideJudgement(regIndex, target, judgement_);
-
-        // const { nonce } = await self.api.query.system.account(self.myself.publicKey);
-        // const myself = self.myself;
-        // /* eslint-disable-next-line */
-        // return new Promise((resolve, reject) => {
-        //   tx.signAndSend(myself, { nonce }, ({ events = [], status }) => {
-        //     console.log('Transaction status:', status.type);
-        //     if (status.isInBlock) {
-        //       console.log('Included at block hash', status.asInBlock.toHex());
-        //       console.log('Events:');
-
-        //       let resp = { blockHash: status.asInBlock.toHex(), events: [] };
-
-        //       events.forEach(({ event: { data, method, section }, phase }) => {
-        //         console.log('\t', phase.toString(), `: ${section}.${method}`, data.toString());
-        //         resp['events'].push(`${phase.toString()}: ${section}.${method}, ${data.toString()}`);
-        //       });
-        //       resolve(resp);
-        //     }
-        //   });
-        // });
-        await self.signAndSend(tx, account);
-        return tx;
-    }
 
     async identitySetFee(account, regIndex = 0, fee = DEFAULT_REGISTRAR_FEE) {
-        // const index = 0;
-        // const fee = 100000000000000;
         const tx = self.api.tx.identity.setFee(regIndex, fee);
         await self.signAndSend(tx, account);
         console.log(`[identity.setFee]: ${tx}`);
@@ -259,7 +133,7 @@ class Chain {
         await self.connect();
         const referendumCount = await self.democracyReferendumCount();
         let referendumInfo = [];
-        for (let i = 0; i < referendumCount; i ++) {
+        for (let i = 0; i < referendumCount; i++) {
             const info = await self.api.query.democracy.referendumInfoOf(i);
             console.log(`[democracy.referendumInfoOf]: ${info}`);
             referendumInfo.push(info);
@@ -268,7 +142,6 @@ class Chain {
         return referendumInfo;
     }
 
-    // async democracyPropose(account, registrarAccount) {
     async democracyPropose(account, func, args, value = DEFAULT_DEMOCRACY_PROPOSAL_FEE) {
         await self.connect();
         // const toAddRegistrar = await self.identityAddRegistrar(registrarAccount);
@@ -304,125 +177,36 @@ class Chain {
                 conviction: 'None',
                 // 0.1 Unit
                 // balance: 1000000000000000
-                balance: balance
-            }
+                balance: balance,
+            },
         };
-        console.log(`vote on referendumInfo: ${referendumInfo[referendumInfo.length-1]}`);
-        const tx = self.api.tx.democracy.vote(referendumInfo.length-1, vote);
+        console.log(`vote on referendumInfo: ${referendumInfo[referendumInfo.length - 1]}`);
+        const tx = self.api.tx.democracy.vote(referendumInfo.length - 1, vote);
         await self.signAndSend(tx, account);
         console.log(`[democracy.vote]: ${tx}`);
         return tx;
     }
-    /**
-     * session
-     */
-    async sessionValidators() {
-        await self.connect();
-        const validators = await self.api.query.session.validators();
-        console.log(`Validators: ${validators.toJSON()}`);
-        return validators;
-    }
-    async sessionDisabledValidators() {
-        await self.connect();
-        const disabledValidators = await self.api.query.session.disabledValidators();
-        console.log(`DisabledValidators: ${disabledValidators.toJSON()}`);
-        return disabledValidators;
-    }
-    async sessionCurrentIndex() {
-        await self.connect();
 
-        const currentIndex = await self.api.query.session.currentIndex();
-        console.log(`[session.currentIndex]: ${currentIndex}`);
-        return currentIndex;
+    async proxyProxies(account) {
+        await self.connect();
+        const resp = await self.api.query.proxy.proxies(account);
+        console.log(`[proxy.proxies]: ${resp}`);
+        return [null, resp];
     }
 
-    async sessionNextKeys() {
+    async proxyAddProxy(account, delegateAccount, proxyType = 'IdentityJudgement', delay = 0) {
         await self.connect();
-        const keys = await self.api.query.session.nextKeys('5GNJqTPyNqANBkUVMN1LPPrxXnFouWXoe2wNSmmEoLctxiZY');
-        console.log(`[session]Next session keys: ${keys}`);
-    }
-
-    async sessionQueuedKeys() {
-        await self.connect();
-        const queuedKeys = await self.api.query.session.queuedKeys();
-        console.log(`[session.queuedKeys]: ${queuedKeys}`);
-        return queuedKeys;
-    }
-    /**
-     * imOnline
-     */
-    async imOnlineHeartbeatAfter() {
-        await self.connect();
-
-        const blockNumber = await self.api.query.imOnline.heartbeatAfter();
-        console.log(`[imOnline.heartBeatAfter]: ${blockNumber}`);
-        return blockNumber;
-    }
-    async imOnlineAuthoredBlocks() {
-        await self.connect();
-        const validatorId = '5GNJqTPyNqANBkUVMN1LPPrxXnFouWXoe2wNSmmEoLctxiZY';
-        const sessionIndex = await self.sessionCurrentIndex();
-        const authorizedBlocks = await self.api.query.imOnline.authoredBlocks(sessionIndex, validatorId);
-        console.log(`[imOnline.authorizedBlocks]: ${authorizedBlocks}`);
-        return authorizedBlocks;
-    }
-    async imOnlineKeys() {
-        await self.connect();
-        const keys = await self.api.query.imOnline.keys();
-        console.log(`[imOnline.keys]: ${keys}`);
-        return keys;
-    }
-    /**
-     * sudo
-     */
-    async sudoKey() {
-        await self.connect();
-        const key = await self.api.query.sudo.key();
-        console.log(`[sudo.key]: ${key}`);
-        return key;
-    }
-    /**
-     * babe
-     */
-    async babeNextRandomness() {
-        await self.connect();
-        const result = await self.api.query.babe.nextRandomness();
-        console.log(`[babe.nextRandomness]: ${result}`);
-        return result;
-    }
-    async babeRandomness() {
-        await self.connect();
-        const result = await self.api.query.babe.randomness();
-        console.log(`[babe.randomness]: ${result}`);
-        return result;
-    }
-    /**
-     * staking
-     */
-    async stakingCurrentEra() {
-        await self.connect();
-        const currentIndex = await self.api.query.staking.currentEra();
-        console.log(`[staking.currentEra]: ${currentIndex}`);
-        return currentIndex;
-    }
-    async stakingEraElectionStatus() {
-        await self.connect();
-        const status = await self.api.query.staking.eraElectionStatus();
-        console.log(`[staking.eraElectionStatus]: ${status}`);
-        return status;
-    }
-    async stakingValidatorCount() {
-        await self.connect();
-        const validatorCount = await self.api.query.staking.validatorCount();
-        console.log(`[staking.validatorCount]: ${validatorCount}`);
-        return validatorCount;
+        const tx = self.api.tx.proxy.addProxy(delegateAccount, proxyType, delay);
+        const resp = await self.signAndSend(tx, account);
+        console.log(`[identity.RequestJudgement] tx: ${tx}`);
+        console.log(`[identity.RequestJudgement] resp: ${resp}`);
+        return [tx, resp];
     }
 
     async disconnect() {
         console.log(`Disconnect from chain`);
         await self.api.disconnect();
     }
-
 
     /**
      * @description set up a registrar for an account
@@ -469,12 +253,12 @@ class Chain {
             referendumInfo = await self.democracyReferendumInfoOf();
         }
         // Extract latest referendum from given array
-        let lastReferendumInfo = referendumInfo[referendumInfo.length-1];
+        let lastReferendumInfo = referendumInfo[referendumInfo.length - 1];
         // Make sure this referendum is `isOngoing` status
-        while (! lastReferendumInfo.value.isOngoing) {
+        while (!lastReferendumInfo.value.isOngoing) {
             await sleep(DEFAULT_SLEEP_INTERVAL);
             referendumInfo = await self.democracyReferendumInfoOf();
-            lastReferendumInfo = referendumInfo[referendumInfo.length-1];
+            lastReferendumInfo = referendumInfo[referendumInfo.length - 1];
         }
         // Now we can safely vote this proposal
         await self.democracyVote(self.alice);
@@ -510,6 +294,7 @@ class Chain {
         await self.identitySetFee(registrarAccount, regIndex, fee);
         await sleep(DEFAULT_SLEEP_INTERVAL);
         await self.identityRegistrars();
+        await self.proxyAddProxy(registrarAccount, self.eve);
     }
 }
 
@@ -517,6 +302,25 @@ class Chain {
     const chain = new Chain(config);
     await chain.connect();
     await chain.setupRegistrar(chain.alice);
-    await chain.setupRegistrar(chain.bob);
+    /* eslint-disable-next-line */
+    const [tx, resp] = await chain.proxyProxies(chain.alice.address);
+    let shouldAddProxy = true;
+    if (resp && resp[0]) {
+        for (let tmp of resp[0]) {
+            const delegateAccount = `${tmp.delegate}`;
+            console.log(delegateAccount);
+            if (delegateAccount === '16D2eVuK5SWfwvtFD3gVdBC2nc2BafK31BY6PrbZHBAGew7L') {
+                shouldAddProxy = false;
+                break;
+            }
+        }
+    }
+    if (shouldAddProxy) {
+        console.log('Should add proxy');
+        await chain.proxyAddProxy(chain.alice, chain.eve.address);
+    } else {
+        console.log('No need to add proxy');
+    }
+
     await chain.disconnect();
 })();
